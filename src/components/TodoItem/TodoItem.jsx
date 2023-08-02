43721007f950088333bcb609da2bb9bb37c55bd8
Fiantso Harena <fiantsorav@gmail.com>
2023-08-02 17:11:18 +0300

    fix: Finish the implementation of the todo task item component

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,7 +1,21 @@
+import { faCircleCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./todoItem.css";
 
-const TodoItem = () => {
-  return <h1>Todo Item</h1>;
+const TodoItem = ({ task, onDelete, onComplete }) => {
+  return (
+    <div className="task">
+      <button className="checkContainer" onClick={() => onComplete(task.id)}>
+        {task.isCompleted ? <FontAwesomeIcon icon={faCircleCheck} /> : <div />}
+      </button>
+
+      <p className={task.isCompleted ? "textCompleted" : ""}>{task.title}</p>
+
+      <button className="deleteButton" onClick={() => onDelete(task.id)}>
+        <FontAwesomeIcon icon={faTrash} />
+      </button>
+    </div>
+  );
 };
 
 export default TodoItem;
