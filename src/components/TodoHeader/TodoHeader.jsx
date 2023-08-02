e8e96b2d2fd85aa111ac4620b0451b78f0e38a13
Fiantso Harena <fiantsorav@gmail.com>
2023-08-02 15:52:27 +0300

    fix: Finish the implementation of the header component

--- src/components/TodoHeader/TodoHeader.jsx
+++ src/components/TodoHeader/TodoHeader.jsx
@@ -13,11 +13,21 @@ const TodoHeader = ({ handleAddTask }) => {
     setTitle("");
   }
 
+  /* onChange event for the input */
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
   return (
     <header className="header">
       <h1 className="logo">Todo</h1>
       <form className="newTaskForm" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Add task" />
+        <input
+          type="text"
+          placeholder="Add task"
+          onChange={onChangeTitle}
+          value={title}
+        />
         <button>
           Create task <FontAwesomeIcon icon={faCirclePlus} size={20} />
         </button>
