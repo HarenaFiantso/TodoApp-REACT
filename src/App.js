5c5d429f920e22972584e140e95c56bed177df1e
Fiantso Harena <fiantsorav@gmail.com>
2023-08-02 15:47:16 +0300

    chore: Implement the add task function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import TodoHeader from "./components/TodoHeader/TodoHeader";
-import "./app.css"
+import { useEffect, useState } from "react";
+import "./app.css";
+
+/* Initialisation of the localstorage key */
+const LOCAL_STORAGE_KEY = "todo:tasks";
 
 const App = () => {
-  return(
+  const [tasks, setTasks] = useState([]);
+
+  /* Useful functions */
+  function loadSavedTasks() {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (saved) {
+      setTasks(JSON.parse(saved));
+    }
+  }
+  function setTasksAndSave(newTasks) {
+    setTasks(newTasks);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks));
+  }
+  useEffect(() => {
+    loadSavedTasks();
+  }, []);
+
+  /* 1st: Adding task function */
+  function addTask(taskTitle) {
+    setTasksAndSave([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false,
+      },
+    ]);
+  }
+
+  return (
     <div>
-      <TodoHeader />
+      <TodoHeader handleAddTask={addTask} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/TodoHeader/TodoHeader.jsx
+++ src/components/TodoHeader/TodoHeader.jsx
@@ -1,12 +1,22 @@
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import "./todoHeader.css";
 
-const TodoHeader = () => {
+const TodoHeader = ({ handleAddTask }) => {
+  const [title, setTitle] = useState("");
+
+  /* onSubmit event for the form */
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleAddTask(title);
+    setTitle("");
+  }
+
   return (
     <header className="header">
       <h1 className="logo">Todo</h1>
-      <form className="newTaskForm">
+      <form className="newTaskForm" onSubmit={handleSubmit}>
         <input type="text" placeholder="Add task" />
         <button>
           Create task <FontAwesomeIcon icon={faCirclePlus} size={20} />
