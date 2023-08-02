8f929fa3e5e252ba7788d5e6e216380e2474b08a
Fiantso Harena <fiantsorav@gmail.com>
2023-08-02 15:02:30 +0300

    chore: Add the font awesome library

--- src/components/TodoHeader/TodoHeader.jsx
+++ src/components/TodoHeader/TodoHeader.jsx
@@ -1,9 +1,11 @@
 import { AiOutlinePlusCircle } from "react-icons/ai";
+
 import "./todoHeader.css";
 
 const TodoHeader = () => {
   return (
     <header className="header">
+      <h1 className="logo">Todo</h1>
       <form className="newTaskForm">
         <input type="text" placeholder="Add task" />
         <button>
