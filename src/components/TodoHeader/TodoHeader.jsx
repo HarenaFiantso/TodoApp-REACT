e74a5b50dc7f9f6108699f79fac9c3fbe01c5af2
Fiantso Harena <fiantsorav@gmail.com>
2023-08-02 14:51:28 +0300

    chore: Initialize the implementation of the TodoHeader component

--- src/components/TodoHeader/TodoHeader.jsx
+++ src/components/TodoHeader/TodoHeader.jsx
@@ -1,7 +1,17 @@
+import { AiOutlinePlusCircle } from "react-icons/ai";
 import "./todoHeader.css";
 
 const TodoHeader = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <form className="newTaskForm">
+        <input type="text" placeholder="Add task" />
+        <button>
+          Create task <AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
+    </header>
+  );
 };
 
 export default TodoHeader;
